3aea24d192d576a11bdb122b1f58830707337212
Moses Ojo <mosesadegoke.jo@gmail.com>
2024-09-16 19:05:31 +0100

    digital-business-card

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 export default function About() {
     return (
-        <section>
+        <section className="about-section">
             <h2 className="about">About</h2>
-            <p classNAme="about-paragraph">
+            <p className="about-paragraph">
                 I am an experienced Frontend Engineer who goes straight to the point with his skills. 
-                I focus on the frontend major and essential problems that needs to be solved using HTML, 
-                CSS, JavaScript and frameworks or library such as ReactJS and jQuery.
+                I focus on the frontend major and essential problems that needs to be solved.
             </p>
         </section>
     )
