79534bcb3fcef32bfc8b39c91a3f865a3613f5dd
Moses Ojo <mosesadegoke.jo@gmail.com>
2024-09-16 19:08:31 +0100

    digital-business-card

--- src/components/Interest.js
+++ src/components/Interest.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Interest() {
     return (
-        <section>
-            <h2>Interests</h2>
-            <p>
+        <section className="interest-section">
+            <h2 className="interest">Interests</h2>
+            <p className="interest-paragraph">
                 I want to be a Philanthropist, giving to the world in the best of my capacity.
                 That is the best way I can code the world into a better place to live.
             </p>
