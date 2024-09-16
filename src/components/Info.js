4c72821c41719688dd86775077a70fb8d1efab54
Moses Ojo <mosesadegoke.jo@gmail.com>
2024-09-16 18:45:26 +0100

    digital-business-card

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,14 +3,20 @@ import UserImage from '../images/user-1.png';
 
 export default function Info() {
     return (
-        <section>
-            <img src={UserImage} alt="User-image"></img>
+        <section className="info-section">
+            <img className="user-image" src={UserImage} alt="User-image"></img>
             <h3>Moses Ojo</h3>
             <p>Frontend Developer</p>
-            <a href='https://github.com/Mosesfamot'>mosesfamot.github</a>
+            <a className="profile-link" href='https://github.com/Mosesfamot'>mosesfamot.github</a>
             <article className="button">
-                <a className="email-button" href='mailto:mosesojo.adegoke@gmail.com'><i class='bx bxs-envelope'></i>Email</a>
-                <a className="linkedin-button" href='https://linkedin.com/mosesojo'><i class='bx bxl-linkedin-square'></i>LinkedIn</a>
+                <a className="email-button" href='mailto:mosesojo.adegoke@gmail.com'>
+                    <i class='bx bxs-envelope'></i>
+                    <span>Email</span>
+                </a>
+                <a className="linkedin-button" href='https://linkedin.com/mosesojo'>
+                    <i class='bx bxl-linkedin-square'></i>
+                    <span>LinkedIn</span>
+                </a>
             </article>
         </section>
     )
